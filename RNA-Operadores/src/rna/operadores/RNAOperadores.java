63c665d175748d10586b8c6928e6cff2b902966d
Zorr0S <86990362+Zorr0S@users.noreply.github.com>
2022-02-09 01:08:50 -0700

    Terminado

--- RNA-Operadores/src/rna/operadores/RNAOperadores.java
+++ RNA-Operadores/src/rna/operadores/RNAOperadores.java
@@ -10,6 +10,7 @@ package rna.operadores;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+
 /**
  *
  * @author alex_
@@ -19,10 +20,14 @@ public class RNAOperadores {
    /**
     * @param args the command line arguments
     */
-   public static int RNA_AND(int[] Input) {
+   public static int RNA_AND(int[] Input ) {
       // Umbral
+      int[] Pesos={1,1};
+      int Total =0;
       
-      int Total = IntStream.of(Input).sum();
+      for (int i = 0; i < Pesos.length; i++) {
+         Total += Input[i] * Pesos[i]; 
+      }
       double Umbral = 1.5;
       if (Total >= Umbral) {
          return 1;
@@ -33,7 +38,12 @@ public class RNAOperadores {
    }
    public static int RNA_OR(int[] Input) {
       // Umbral
-      int Total = IntStream.of(Input).sum();
+      int[] Pesos={1,1};
+      int Total =0;
+      
+      for (int i = 0; i < Pesos.length; i++) {
+         Total += Input[i] * Pesos[i]; 
+      }
       double Umbral = 0.5;
       if (Total >= Umbral) {
          return 1;
@@ -42,8 +52,8 @@ public class RNAOperadores {
       }
    }
    public static int RNA_XOR(int[] Input) {
-    
-      int[] Totales = {(RNA_AND(Input))*-1,RNA_OR(Input)*1};
+      
+      int[] Totales = {(RNA_AND(Input ))*-1,RNA_OR(Input)*1};
       int Total=  IntStream.of(Totales).sum();
       
       // Umbral
@@ -56,63 +66,46 @@ public class RNAOperadores {
    }
 
 
+   public static int RNA_NOT(int Input){
+      
+      int[] Pesos={-2};
+      int Total =0;
+      
+      for (int i = 0; i < Pesos.length; i++) {
+         Total += Input * Pesos[i]; 
+      }
+      
+      double Umbral = -1;
+      if (Total >= Umbral) {
+         return 1;
+      } else {
+         return 0;
+      }
 
+   }
 
    public static void main(String[] args) {
-      System.out.println("\n ----------------- Operador AND ----------------- ");
-      int[] Intput = { 0, 0 };
-      Intput[0] = 1;
-      Intput[1] = 1;
-      System.out.println( Arrays.toString(Intput) +" AND : " + RNA_AND(Intput));
       
-      Intput[0] = 0;
-      Intput[1] = 1;
-      System.out.println( Arrays.toString(Intput) +" AND : " + RNA_AND(Intput));
+      int[][] Intput ={ { 1, 1 },
+                        { 1, 0 },
+                        { 0, 1 },
+                        { 0, 0 }};
+   
       
-      Intput[0] = 1;
-      Intput[1] = 0;
-      System.out.println( Arrays.toString(Intput) +" AND : " + RNA_AND(Intput));
+      System.out.println( "[ "+Intput[1][0]+" ]" +" NOT : " + RNA_NOT(Intput[1][0]));
+      System.out.println( "[ "+Intput[1][1]+" ]" +" NOT : " + RNA_NOT(Intput[1][1]));
+      System.out.println();
+      for (int i = 0; i < Intput.length; i++) {
+         System.out.println( Arrays.toString(Intput[i]) +" AND : " + RNA_AND(Intput[i]));
+         System.out.println( Arrays.toString(Intput[i]) +" OR : " + RNA_OR(Intput[i]));
+         
+         System.out.println( Arrays.toString(Intput[i]) +" XOR : " + RNA_XOR(Intput[i]));
+        
+         System.out.println( "[ "+Intput[i][1]+" ]" +" NOT : " + RNA_NOT(Intput[i][1]));
+         
+         System.out.println("\n");
+      }
       
-      Intput[0] = 0;
-      Intput[1] = 0;
-      System.out.println( Arrays.toString(Intput) +" AND : " + RNA_AND(Intput));
-      
-      System.out.println("\n ----------------- Operador OR ----------------- ");
-
-      Intput[0] = 1;
-      Intput[1] = 1;
-      System.out.println( Arrays.toString(Intput) +" OR : " + RNA_OR(Intput));
-      
-      Intput[0] = 0;
-      Intput[1] = 1;
-      System.out.println( Arrays.toString(Intput) +" OR : " + RNA_OR(Intput));
-      
-      Intput[0] = 1;
-      Intput[1] = 0;
-      System.out.println( Arrays.toString(Intput) +" OR : " + RNA_OR(Intput));
-      
-      Intput[0] = 0;
-      Intput[1] = 0;
-      System.out.println( Arrays.toString(Intput) +" OR : " + RNA_OR(Intput));
-
-      System.out.println("\n ----------------- Operador XOR ----------------- ");
-
-      Intput[0] = 1;
-      Intput[1] = 1;
-      System.out.println( Arrays.toString(Intput) +" XOR : " + RNA_XOR(Intput));
-      
-      Intput[0] = 0;
-      Intput[1] = 1;
-      System.out.println( Arrays.toString(Intput) +" XOR : " + RNA_XOR(Intput));
-      
-      Intput[0] = 1;
-      Intput[1] = 0;
-      System.out.println( Arrays.toString(Intput) +" XOR : " + RNA_XOR(Intput));
-      
-      Intput[0] = 0;
-      Intput[1] = 0;
-      System.out.println( Arrays.toString(Intput) +" XOR : " + RNA_XOR(Intput));
-
       
    }
 
