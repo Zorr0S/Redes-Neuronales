404765627b5886e1a58f46ff93afff6f87983838
Zorr0S <86990362+Zorr0S@users.noreply.github.com>
2022-02-10 20:27:36 -0700

    Multiplicacion

--- RNA MAtrix/src/Principal/App.java
+++ RNA MAtrix/src/Principal/App.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+// 3.6 2-mayo
+// Aplication to the financial  markets
 package Principal;
 
 import java.util.Arrays;
@@ -19,6 +20,7 @@ public class App {
     /**
      * @param args the command line arguments
      */
+
     public static int RNA_AND(Matrix Input) {
         // Umbral
         double[] Pesos = { 1, 1 };
@@ -83,6 +85,32 @@ public class App {
         }
 
     }
+    public static boolean RNAComparar(Matrix Entrada){
+        ///                N1 - N2- N3- N4- N5- N6
+        double[][] Red = {{ 0 , 1 ,-1 , 1, -1, 1},   //N1
+                          { 1 , 0 ,-1 , 1, -1, 1},   //N2
+                          {-1 ,-1 , 0 ,-1,  1,-1},  //N3
+                          { 1 , 1 ,-1 , 0, -1, 1},  //N4
+                          {-1 ,-1 , 1 ,-1,  0,-1},  //N5
+                          { 1 , 1 ,-1 , 1,- 1, 0},  //N6
+    
+                        };
+        
+        System.out.println(Arrays.toString(Red));               
+        Matrix redMatrix= new Matrix(Red);
+        Matrix resultadoMatrix = MatrixMath.multiply(Entrada, redMatrix);
+
+        if(resultadoMatrix.equals(Entrada)){
+           System.out.println("Es reconocido");
+            return true;
+        }else{
+           System.out.println("no es reconocido");
+
+            return false;
+        }
+        
+        
+    }
 
     public static void main(String[] args) {
 
@@ -90,7 +118,15 @@ public class App {
                 { 1, 0 },
                 { 0, 1 },
                 { 0, 0 } };
-
+        double[][] Muestra= {{0,0,1,0,1,0},
+                             {1,1,0,1,0,1},
+                            { 1,0,1,0,1,1}
+                        };
+        Matrix MuestraMatrix= new Matrix(Muestra);
+        for (int i = 0; i < MuestraMatrix.getRows(); i++) {
+            RNAComparar(MuestraMatrix.getRow(i));
+            
+        }
         Matrix Entrada = new Matrix(Intput);
 
         System.out.println("[ " + Intput[1][0] + " ]" + " NOT : " + RNA_NOT(Entrada.get(1,0)));
---
+++ Trabajo en CLae/Trabajo en Clase/src/main/App.java
@@ -0,0 +1,8 @@
+package main;
+import neural.matrix.Matrix;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
